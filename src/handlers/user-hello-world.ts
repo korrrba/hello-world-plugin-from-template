fad20ea7cac2d5dfbfcb2c0b4307290e45932329
korrrba <88761781+gitcoindev@users.noreply.github.com>
2024-06-30 23:26:31 +0200

    Create user-hello-world.ts

---
+++ src/handlers/user-hello-world.ts
@@ -0,0 +1,20 @@
+import { Context } from "../types";
+import { addCommentToIssue } from "../utils/issue";
+
+export async function userStartStop(context: Context): Promise<{ output: string | null }> {
+  const { payload, logger, config } = context;
+  const { issue, comment, sender, repository } = payload as Context<"issue_comment.created">["payload"];
+  const directive = comment.body.split(" ")[0].replace("/", "");
+  const { disabledCommands } = config;
+  const isCommandDisabled = disabledCommands.some((command: string) => command === directive);
+
+  if (isCommandDisabled) {
+    await addCommentToIssue(context, "```diff\n! The /start command is disabled for this repository.\n```");
+    throw logger.error(`The '/${directive}' command is disabled for this repository.`);
+  }
+
+  if (directive === "hello") {
+    await addCommentToIssue(context, "```Hello world plugin from template!\n```");
+  };
+  return { output: null };
+}
