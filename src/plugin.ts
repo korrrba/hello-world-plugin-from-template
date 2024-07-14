8dfc86bdb2688b2dd383f81e2e5eb8447b6352ac
Korrrba <gitcoiner@gmail.com>
2024-07-14 22:43:44 +0200

    chore: simplify plugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,5 +1,4 @@
 import { Octokit } from "@octokit/rest";
-import { createClient } from "@supabase/supabase-js";
 import { createAdapters } from "./adapters";
 import { Env, PluginInputs } from "./types";
 import { Context } from "./types";
@@ -10,7 +9,6 @@ import { userHelloWorld } from "./handlers/user-hello-world";
  */
 export async function plugin(inputs: PluginInputs, env: Env) {
   const octokit = new Octokit({ auth: inputs.authToken });
-  const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
 
   const context: Context = {
     eventName: inputs.eventName,
@@ -38,10 +36,8 @@ export async function plugin(inputs: PluginInputs, env: Env) {
     adapters: {} as ReturnType<typeof createAdapters>,
   };
 
-  context.adapters = createAdapters(supabase, context);
-
   if (context.eventName === "issue_comment.created") {
-    data = await userHelloWorld(context);
+    await userHelloWorld(context);
   } else {
     context.logger.error(`Unsupported event: ${context.eventName}`);
   }
