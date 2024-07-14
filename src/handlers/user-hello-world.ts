790f25f9af62902e009cfa233cf7ad4c54a21a88
Korrrba <gitcoiner@gmail.com>
2024-07-14 23:03:23 +0200

    chore: hellow world

--- src/handlers/user-hello-world.ts
+++ src/handlers/user-hello-world.ts
@@ -2,16 +2,9 @@ import { Context } from "../types";
 import { addCommentToIssue } from "../utils/issue";
 
 export async function userHelloWorld(context: Context): Promise<{ output: string | null }> {
-  const { payload, logger, config } = context;
+  const { payload } = context;
   const { comment } = payload as Context<"issue_comment.created">["payload"];
   const directive = comment.body.split(" ")[0].replace("/", "");
-  const { disabledCommands } = config;
-  const isCommandDisabled = disabledCommands.some((command: string) => command === directive);
-
-  if (isCommandDisabled) {
-    await addCommentToIssue(context, "```diff\n! The /start command is disabled for this repository.\n```");
-    throw logger.error(`The '/${directive}' command is disabled for this repository.`);
-  }
 
   if (directive === "hello") {
     await addCommentToIssue(context, "```Hello world plugin from template!\n```");
